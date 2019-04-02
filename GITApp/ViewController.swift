14ffa279ff65c6c335a7e71c990ecbf71f7f6c0c
Anson Lee <alee2022@208-engineering-11.local>
2019-04-02 09:09:59 -0700

    Initial Commit

---
+++ GITApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GITApp
+//
+//  Created by Anson Lee on 4/2/19.
+//  Copyright © 2019 Anson Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
